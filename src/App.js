c37347d9af2e6b572c1d86bc2c45c19943c50838
PiotrBerebecki <petebere@gmail.com>
2017-02-01 16:33:51 +0000

    Handle clicks on progress bars

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ class App extends Component {
     super();
     this.togglePlay = this.togglePlay.bind(this);
     this.handleProgress = this.handleProgress.bind(this);
+    this.handleRangeUpdate = this.handleRangeUpdate.bind(this);
+    this.scrub = this.scrub.bind(this);
     this.state = {
       video: null,
-      progress: '0%'
+      progress: '0%',
+      playbackRate: 1,
+      volume: 1,
     };
   }
   
@@ -36,13 +40,30 @@ class App extends Component {
     const { video } = this.state;
     const percent = (video.currentTime / video.duration) * 100;
     this.setState({
-      progress: percent
+      progress: `${percent}%`
     });
   }
   
+  handleRangeUpdate(e) {
+    console.log('range update');
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+    // Todo: Check how to update state with Immutable JS
+    // instead of using refs
+    this.refs.video[name] = value;
+  }
+  
+  scrub(e) {
+    const scrubTime = (e.nativeEvent.offsetX / e.target.offsetWidth) * this.state.video.duration;
+    // Todo: Check how to update state with Immutable JS
+    // instead of using refs
+    this.refs.video.currentTime = scrubTime;
+  }
+  
   render() {
-    const { video } = this.state;
-    // console.dir(this.state.video);
+    const { video, progress, playbackRate, volume } = this.state;
     
     return (
       
@@ -51,17 +72,20 @@ class App extends Component {
         <video
           className="player__video viewer"
           ref="video"
-          autoPlay
+          // autoPlay
           src="https://player.vimeo.com/external/194837908.sd.mp4?s=c350076905b78c67f74d7ee39fdb4fef01d12420&profile_id=164"
           onClick={this.togglePlay}
         />
         
         <div className="player__controls">
 
-          <div className="progress">
+          <div 
+            className="progress"
+            onClick={this.scrub}
+          >
            <div
              className="progress__filled"
-             style={{'flexBasis': this.state.progress}}
+             style={{'flexBasis': progress}}
            ></div>
           </div>
           
@@ -69,14 +93,35 @@ class App extends Component {
             className="player__button toggle" 
             title="Toggle Play"
             onClick={this.togglePlay}>
-            { this.state.video && this.state.video.paused ? '►' : '❚ ❚' }
+            { video && video.paused ? '►' : '❚ ❚' }
           </button>
           
-          <input type="range" name="volume" className="player__slider" min="0" max="1" step="0.05"/>
-          <input type="range" name="playbackRate" className="player__slider" min="0.5" max="2" step="0.1"/>
+          <input 
+            type="range" 
+            name="volume" 
+            className="player__slider" 
+            min="0" max="1" step="0.05" value={volume}
+            onChange={this.handleRangeUpdate}
+          />
+          <input 
+            type="range" 
+            name="playbackRate" 
+            className="player__slider" 
+            min="0.5" max="2" step="0.1" value={playbackRate}
+            onChange={this.handleRangeUpdate}
+          />
           
-          <button data-skip="-10" className="player__button">« 10s</button>
-          <button data-skip="25" className="player__button">25s »</button>
+          <button 
+            data-skip="-10" 
+            className="player__button"
+          >« 10s
+          </button>
+          
+          <button 
+            data-skip="25" 
+            className="player__button"
+          >25s »
+          </button>
           
         </div>
 
